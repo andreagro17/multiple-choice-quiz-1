b77f870fcf967d55a0400bf76a4b5ed710e8d130
acastros <acastros@minsait.com>
2024-02-25 11:45:52 +0100

    Multiple choice quiz app

--- src/App.vue
+++ src/App.vue
@@ -1,2 +1,13 @@
-<script setup></script>
-<template></template>
+<script setup>
+import Quiz from "./Quiz.vue";
+</script>
+
+<template>
+	<div id="app">
+		<div class="h-screen w-screen flex items-center justify-center text-white">
+			<div class="max-w-7xl flex items-center justify-center">
+				<Quiz />
+			</div>
+		</div>
+	</div>
+</template>
